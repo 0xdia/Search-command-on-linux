cdd36db1f6f8e5b17444b587e380b79b2e9a0e29
Dhiaa <joseph.zebbiche@gmail.com>
2021-06-13 22:15:49 +0100

    Implement basic functionalities of "affichage.h"

--- code/affichage.h
+++ code/affichage.h
@@ -1,58 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
 short DATE_CREATION       = 0;
-short DATE_DENRIER_ACCES  = 0;
+short DATE_DERNIER_ACCES  = 0;
 short DATE_DERNIERE_MODIF = 0;
 short TAILLE              = 0;
 short TYPE                = 0;
 short PROTECTION          = 0;
 short ALL                 = 0;
 
-void afficher(struct stat *s)
+
+void print_protection(struct stat *s)
 {
-    if (ALL) {
-        printf("Protection  Taille  Type  Dernier acces  Dernière modification  Date de mréation\n");  
-    }
-    else {
-        if (PROTECTION)
-            printf("Protection ");
-        if (TAILLE)
-            printf("Taille ");
-        if (TYPE)
-            printf("Type ");
-        if (DATE_DENRIER_ACCES)
-            printf("Dernier acces ");
-        if (DATE_DERNIERE_MODIF)
-            printf("Dernier modification ");
-        if (DATE_CREATION)
-            printf("Date de création");
+    // re-write this function later
+    printf("%3o      ", s->st_mode&0777);
+}
 
-        puts("");
-    }
+void print_type(struct stat *s) // Search for name conventions in french in the course
+{
+    mode_t t = s->st_mode;
+    if (S_ISREG(t))
+        printf("Fichier régulier ");
+    if (S_ISFIFO(t))
+        printf("Fichier spécial ");
+    if (S_ISCHR(t))
+        printf("Périph mode caractère ");
+    if (S_ISBLK(t))
+        printf("Périph mode bloc ");
+    if (S_ISDIR(t))
+        printf("Répertoire ");
+    if (S_ISLNK(t))
+        printf("Lien symbolique ");
+    if (S_ISSOCK(t))
+        printf("Socket ");
+}
 
-    if (ALL) {
-        print_all(s);
-    }
-    else {
-        if (PROTECTION)
-            print_protection();
-        if (TAILLE)
-            print_taille();
-        if (TYPE)
-            print_type();
-        if (DATE_DENRIER_ACCES)
-            print_dernier_acces();
-        if (DATE_DERNIERE_MODIF)
-            print_derniere_modif();
-        if (DATE_CREATION)
-            print_date_creation();
+void print_dernier_acces(struct stat *s)
+{
+    printf("%ld ", s->st_atime); 
+}
 
-        puts("");
-    }
+void print_derniere_modif(struct stat *s)
+{
+    printf("%ld ", s->st_mtime);
+}
 
+void print_date_creation(struct stat *s)
+{
+    // how to obtain this ?
+    printf("Birth date");
+}
+
+void print_taille(struct stat *s)
+{
+    printf("%ld ", s->st_size);
 }
 
 void print_all(struct stat *s)
@@ -65,53 +70,74 @@ void print_all(struct stat *s)
     puts("");
 }
 
-void print_protection(struct stat *s)
-{
-    // re-write this function later
-    printf("%3o ", s->st_mode&0777);
-}
 
-void print_type(struct stat *s) // Search for name conventions in french in the course
+void afficher_entete()
 {
-    switch (s->st_mode & S_IFMT) {
-        case S_IFBLK:
-            printf("Block device node ");
-            break;
-        case S_IFCHR:
-            printf("Character device node ");
-            break;
-        case S_IFDIR:
-            printf("Directory ");
-            break;
-        case S_IFIFO:
-            printf("FIFO ");
-            break;
-        case S_IFLNK:
-            printf("Symbolic link ");
-            break;
-        case S_IFREG:
-            printf("Regular file ");
-            break;
-        case S_IFSOCK:
-            printf("Socket ");
-            break;
-        default:
-            printf("Unkown ");
-            break;
+    if (ALL) {
+        printf("Protection  Taille  Type  Dernier acces  Dernière modification  Date de création\n");  
+    }
+    else {
+        if (PROTECTION)
+            printf("Protection ");
+        if (TAILLE)
+            printf("Taille ");
+        if (TYPE)
+            printf("Type ");
+        if (DATE_DERNIER_ACCES)
+            printf("Dernier acces ");
+        if (DATE_DERNIERE_MODIF)
+            printf("Dernier modification ");
+        if (DATE_CREATION)
+            printf("Date de création");
+
+        puts("");
     }
 }
 
-void print_dernier_acces(struct stat *s)
+
+void afficher(struct stat *s)
 {
-    printf("%lld ", s->st_atime); 
+    afficher_entete();
+
+    if (ALL) {
+        print_all(s);
+    }
+    else {
+        if (PROTECTION)
+            print_protection(s);
+        if (TAILLE)
+            print_taille(s);
+        if (TYPE)
+            print_type(s);
+        if (DATE_DERNIER_ACCES)
+            print_dernier_acces(s);
+        if (DATE_DERNIERE_MODIF)
+            print_derniere_modif(s);
+        if (DATE_CREATION)
+            print_date_creation(s);
+
+        puts("");
+    }
+
 }
 
-void print_derniere_modif(struct stat *s)
+void specifier_parametres(char* param[], int n)
 {
-    printf("%lld ", s->mtime);
-}
+    for (int i=1; i<n; i++) {
+        if (! strcmp(param[i], "-d"))
+            DATE_CREATION = DATE_DERNIER_ACCES = 1;
+        if (! strcmp(param[i], "-m"))
+            DATE_DERNIERE_MODIF = 1;
+        if (! strcmp(param[i], "-s"))
+            TAILLE = 1;
+        if (! strcmp(param[i], "-t"))
+            TYPE   = 1;
+        if (! strcmp(param[i], "-p"))
+            PROTECTION = 1;
+        if (! strcmp(param[i], "-a"))
+            ALL = 1;
+    }
 
-void print_date_creation(struct stat *s)
-{
-    // how to obtain this ?
+    if (!(ALL||DATE_CREATION||DATE_DERNIER_ACCES||DATE_DERNIERE_MODIF||TAILLE||TYPE||PROTECTION))
+        ALL = 1;
 }
