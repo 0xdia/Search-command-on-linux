c881a1e9bee2903393773f6f00e19b762f6e3862
Dhiaa <joseph.zebbiche@gmail.com>
2021-06-15 20:21:47 +0100

    Implement 'Correspondre' for pattern matching

--- code/patterns.h
+++ code/patterns.h
@@ -4,9 +4,12 @@
 
 void nettoyer_nom(char* pattern)
 {
+    if (strlen(pattern) == 1)
+        return;
+
     char propre[256];
     propre[0] = pattern[0]; 
-
+    
     // enlever les '*' consécutives
     int j = 1;
     for (int i=1; i<strlen(pattern); i++)
@@ -19,8 +22,6 @@ void nettoyer_nom(char* pattern)
     }
 
     propre[j] = '\0';
-
-    // * following ? = ? following * = +
 
     strcpy(pattern, propre);
 }
@@ -36,23 +37,41 @@ int go(char* p, char *b)
 
 int correspondre(char* p, int cur_p, char* b, int cur_b) 
 {
+    // printf("[***]: %s x %s\n", &p[cur_p], &b[cur_b]);
+    
+    if (strcmp(&p[cur_p], &b[cur_b]) == 0) {
+        puts("Done");
+        return 1;
+    }
+
+    if (p[cur_p] == '\0' && b[cur_b] != '\0') {
+        // puts(" NO");
+        return 0;
+    }
+
     if (p[cur_p] == '*') {
-        if (b[cur_b+1] == '\0')
+        if (p[cur_p+1] == '\0')
             return 1;
 
         int at_least_at_the_end = 0;
         for (int i=cur_p+1; p[i] != '\0'; i++)
             at_least_at_the_end += (p[i] != '*');
+    
+        // printf(" at least at the end = %d\n", at_least_at_the_end);
 
-        int chars_remaining = 0;
-        for (int i=cur_b, chars_remaining; b[i] != '\n'; i++, chars_remaining++)
-        ;
+        int chars_remaining = strlen(b) - cur_b;
+        
+        //for (int i=cur_b, chars_remaining; b[i] != '\n'; i++, chars_remaining++)
+        //;
+
+        // printf(" chars remaining = %d\n", chars_remaining);
 
         if (at_least_at_the_end > chars_remaining)
             return 0;
 
+        // needs a clever optimization using DP
         for (; b[cur_b] != '\0'; cur_b++)
-            if (correspondre(p, cur_p, b, cur_b))
+            if (correspondre(p, cur_p+1, b, cur_b))
                 return 1;
 
         return 0;
@@ -64,12 +83,22 @@ int correspondre(char* p, int cur_p, char* b, int cur_b)
 
         if (b[cur_b+1] == '\0')
             return 0;
-
-        return correspondre(p, cur_p+1, b, cur_b+1);
     }
+    
+    else {
+        // printf("Testing 2 chars: ");
+        if (p[cur_p] != b[cur_b]) {
+        //    puts("No");
+            return 0;
+        }
 
-    if (p[cur_p] != b[cur_b+1])
-        return 0;
+        if (p[cur_p+1] == b[cur_b+1] && p[cur_p+1] == '\0') {
+        //    puts("Yes");
+            return 1;
+        }
+
+        // puts("Yes");
+    }
 
     return correspondre(p, cur_p+1, b, cur_b+1);
 }
--- code/test_patterns.c
+++ code/test_patterns.c
@@ -9,15 +9,8 @@ int main(int argc, char** argv)
 
     nettoyer_nom(argv[1]);
 
-    printf("%s: %ld\n", argv[1], strlen(argv[1]));
-    if (go(argv[1], argv[2]))
-        puts("Go!");
-    else puts("No Go!");
-
-    puts("");
-    if (correspondre(argv[1], 0, argv[2], 0))
-        puts("Yes!");
-    else puts("No!");
+    if (! go(argv[1], argv[2])) {
+        return 0;
 
     return 0;
 }
